e050bf18bd237b15ffd241592aef935c29ebc42e
Anuj kumar <theanujsinha@gmail.com>
2025-02-07 17:48:02 +0530

    commit

---
+++ TYPES OF COMMANDS/DML.sql
@@ -0,0 +1,18 @@
+-- DML --data manipulation language
+--> -- INSERT into , UPDATE , DELETE
+
+------------------------------------------------------------------------------------------
+-- to insert data into a table
+INSERT INTO [TABLE_NAME] ([COLUMN_NAME], [COLUMN_NAME], [COLUMN_NAME]) VALUES ([VALUE], [VALUE], [VALUE]);
+
+------------------------------------------------------------------------------------------
+
+-- to update data in a table
+UPDATE [TABLE_NAME] SET [COLUMN_NAME] = [VALUE] WHERE [CONDITION];
+
+------------------------------------------------------------------------------------------
+
+-- to delete data from a table
+DELETE FROM [TABLE_NAME] WHERE [CONDITION];
+
+------------------------------------------------------------------------------------------
