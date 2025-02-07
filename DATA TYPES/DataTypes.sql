248285fe8ae9c0bccfa7748f5570879d110de559
Anuj kumar <theanujsinha@gmail.com>
2025-02-07 17:32:31 +0530

    commit

--- DATA TYPES/DataTypes.sql
+++ DATA TYPES/DataTypes.sql
@@ -1,7 +1,7 @@
 -- string types
-char(10) -- fixed length string
-varchar(10) -- variable length string
-text -- variable length string
+char(10) -- fixed length string 
+varchar(10) -- variable length string 
+text -- variable length string  
 
 ------------------------------------------------------------------------------------------
 -- numeric types
@@ -13,13 +13,14 @@ boolean -- true or false
 ------------------------------------------------------------------------------------------
 
 -- date types
-date -- date
-time -- time
-datetime -- date and time
+date -- date yyyy-mm-dd
+time -- time hh:mm:ss
+datetime -- date and time  yyyy-mm-dd hh:mm:ss
+
 
 ------------------------------------------------------------------------------------------
 
 -- other types
 binary -- binary data exampel: binary(10)
-enum -- enumeration  exampel: enum('a', 'b', 'c')
+enum -- enumeration  exampel: enum('a', 'b', 'c') can only store one of these values
 
