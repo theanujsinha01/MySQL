33d8a3a09de1cea0fccb938a6a8e9293bc238ae8
Anuj kumar <theanujsinha@gmail.com>
2025-02-08 18:23:15 +0530

    commit

---
+++ STRUCTURE OF SQL QUERY/Structure_of_query.sql
@@ -0,0 +1,10 @@
+-- structure of  sql query
+
+SELECT column1, column2, column3, column4
+FROM table_name
+WHERE condition1 AND condition2 AND condition3 ...;
+GROUP BY column1, column2, column3 ...;
+HAVING condition1 AND condition2 AND condition3 ...;
+ORDER BY column1, column2, column3 ...;
+LIMIT number_of_rows;
+
