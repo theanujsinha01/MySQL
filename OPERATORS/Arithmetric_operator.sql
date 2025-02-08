da1620efcbb72b44abadb6f12c0ad7ce4eceed84
Anuj kumar <theanujsinha@gmail.com>
2025-02-08 18:05:19 +0530

    commit

---
+++ OPERATORS/Arithmetric_operator.sql
@@ -0,0 +1,15 @@
+
+-- + is used for addition
+-- - is used for subtraction
+-- * is used for multiplication
+-- / is used for division
+-- % is used for modulus
+
+SELECT 5 + 3 AS Addition;
+SELECT 5 - 3 AS Subtraction;
+SELECT 5 * 3 AS Multiplication;
+SELECT 5 / 3 AS Division;
+SELECT 5 % 3 AS Modulus;
+
+
+
---
+++ OPERATORS/Comparison_operator.sql
@@ -0,0 +1,13 @@
+-- = , > ,< , >= , <=
+-- <> , != , !< , !>
+
+SELECT * FROM employees WHERE salary > 10000; -- Greater than
+SELECT * FROM employees WHERE salary < 10000; -- Less than
+SELECT * FROM employees WHERE salary >= 10000; -- Greater than or equal to
+SELECT * FROM employees WHERE salary <= 10000; -- Less than or equal to
+SELECT * FROM employees WHERE salary = 10000; -- Equal to
+SELECT * FROM employees WHERE salary <> 10000; -- Not equal to
+SELECT * FROM employees WHERE salary != 10000; -- Not equal to
+SELECT * FROM employees WHERE salary !< 10000; -- Not less than
+SELECT * FROM employees WHERE salary !> 10000; -- Not greater than
+
