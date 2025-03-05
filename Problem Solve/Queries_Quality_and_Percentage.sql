9d1981e7a78f3eb8d52c6597ab1b4911a7c308f4
Anuj kumar <theanujsinha@gmail.com>
2025-03-05 18:13:09 +0530

    COMMIT

--- Problem Solve/Queries_Quality_and_Percentage.sql
+++ Problem Solve/Queries_Quality_and_Percentage.sql
@@ -64,7 +64,7 @@ Cat queries poor_ query_percentage is (1 / 3) * 100 = 33.33
 
 solution:
 SELECT query_name,
-       ROUND(SUM(rating/position) / COUNT(*), 2) AS quality,
-       ROUND(SUM(IF(rating < 3, 1, 0)) * 100 / COUNT(*), 2) AS poor_query_percentage
+       ROUND(AVG(rating/position), 2) AS quality,
+       ROUND(AVG(rating < 3)*100,2) AS poor_query_percentage
 FROM Queries
 GROUP BY query_name;
